e3717d74ba7803bcb34ac1bcd905d334ad9c7b36
cmdct <sylvain.demaimay@commandersact.com>
2020-12-29 17:33:26 +0100

    Avoid escaping quotes

--- js/content.js
+++ js/content.js
@@ -1,127 +1,127 @@
 const content = {
   home: {
-    title: 'Liens',
+    title: "Liens",
     cards: [
       {
-        title: 'Langage',
+        title: "Langage",
         links: [
-          {name: 'Tech', href: 'https://techterms.com'},
-          {name: 'Marketing', href: 'https://www.definitions-marketing.com'},
-          {name: 'Conjugueur', href: 'https://www.gymglish.com/fr/conjugaison/vatefaireconjuguer'},
-          {name: 'Wiktionary', href: 'https://www.wiktionary.org'},
-          {name: 'Wikipedia', href: 'https://www.wikipedia.org'},
-          {name: 'Translate', href: 'https://translate.google.com/?ui=tob&sl=en&tl=fr&op=translate'}
+          {name: "Tech", href: "https://techterms.com"},
+          {name: "Marketing", href: "https://www.definitions-marketing.com"},
+          {name: "Conjugueur", href: "https://www.gymglish.com/fr/conjugaison/vatefaireconjuguer"},
+          {name: "Wiktionary", href: "https://www.wiktionary.org"},
+          {name: "Wikipedia", href: "https://www.wikipedia.org"},
+          {name: "Translate", href: "https://translate.google.com/?ui=tob&sl=en&tl=fr&op=translate"}
         ]
       },
       {
-        title: 'Gestion',
+        title: "Gestion",
         links: [
-          {name: 'Lucca', href: 'https://fjordtechnologies.ilucca.net/home'},
-          {name: 'Zoho', href: 'https://care.commandersact.com/support/commandersact529'},
-          {name: 'Slack', href: 'https://tagcoteam.slack.com'},
-          {name: 'Office', href: 'https://login.microsoftonline.com'},
-          {name: 'Spendek', href: 'https://app.spendesk.com/auth/login'}
+          {name: "Lucca", href: "https://fjordtechnologies.ilucca.net/home"},
+          {name: "Zoho", href: "https://care.commandersact.com/support/commandersact529"},
+          {name: "Slack", href: "https://tagcoteam.slack.com"},
+          {name: "Office", href: "https://login.microsoftonline.com"},
+          {name: "Spendek", href: "https://app.spendesk.com/auth/login"}
         ]
       },
       {
-        title: 'Solution',
+        title: "Solution",
         links: [
-          {name: 'Platform', href: 'https://platform.commandersact.com'},
-          {name: 'Extension', href: 'https://chrome.google.com/webstore/detail/commanders-act-assistant/lfaifjhjdolnpnlgeohohaalbeidhlpj?hl=fr'},
-          {name: 'Documentation', href: 'https://community.commandersact.com/en/'},
-          {name: 'Corporate', href: 'https://www.commandersact.com'}
+          {name: "Platform", href: "https://platform.commandersact.com"},
+          {name: "Extension", href: "https://chrome.google.com/webstore/detail/commanders-act-assistant/lfaifjhjdolnpnlgeohohaalbeidhlpj?hl=fr"},
+          {name: "Documentation", href: "https://community.commandersact.com/en/"},
+          {name: "Corporate", href: "https://www.commandersact.com"}
         ]
       },
       {
-        title: 'Client',
+        title: "Client",
         links: [
-          {name: 'DevTools', href: 'https://developers.google.com/web/tools/chrome-devtools'},
-          {name: 'Status Codes', href: 'https://developer.mozilla.org/en-US/docs/Web/HTTP/Status'},
-          {name: 'Check IP', href: 'http://checkip.amazonaws.com/'},
-          {name: 'Masque', href: 'https://cric.grenoble.cnrs.fr/Administrateurs/Outils/CalculMasque'}
+          {name: "DevTools", href: "https://developers.google.com/web/tools/chrome-devtools"},
+          {name: "Status Codes", href: "https://developer.mozilla.org/en-US/docs/Web/HTTP/Status"},
+          {name: "Check IP", href: "http://checkip.amazonaws.com/"},
+          {name: "Masque", href: "https://cric.grenoble.cnrs.fr/Administrateurs/Outils/CalculMasque"}
         ]
       },
       {
-        title: 'Code',
+        title: "Code",
         links: [
-          {name: 'Beautifier', href: 'https://beautifier.io'},
-          {name: 'DiffNow', href: 'https://www.diffnow.com/compare-clips'},
-          {name: 'Codeshare', href: 'https://codeshare.io'}
+          {name: "Beautifier", href: "https://beautifier.io"},
+          {name: "DiffNow", href: "https://www.diffnow.com/compare-clips"},
+          {name: "Codeshare", href: "https://codeshare.io"}
         ]
       },
       {
-        title: 'JavaScript',
+        title: "JavaScript",
         links: [
-          {name: 'MDN', href: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript'},
-          {name: 'Eloquent', href: 'https://eloquentjavascript.net/'},
-          {name: 'ECMAScript', href: 'https://www.ecma-international.org/ecma-262/11.0/index.html#title'}
+          {name: "MDN", href: "https://developer.mozilla.org/en-US/docs/Web/JavaScript"},
+          {name: "Eloquent", href: "https://eloquentjavascript.net/"},
+          {name: "ECMAScript", href: "https://www.ecma-international.org/ecma-262/11.0/index.html#title"}
         ]
       },
       {
-        title: 'CSS',
+        title: "CSS",
         links: [
-          {name: 'Selectors', href: 'https://www.w3schools.com/cssref/css_selectors.asp'},
-          {name: 'Flexbox', href: 'https://css-tricks.com/snippets/css/a-guide-to-flexbox/'},
-          {name: 'Colors', href: 'https://en.wikipedia.org/wiki/Web_colors'},
-          {name: 'ColorZilla', href: 'https://chrome.google.com/webstore/detail/colorzilla/bhlhnicpbhignbdhedgjhgdocnmhomnp'}
+          {name: "Selectors", href: "https://www.w3schools.com/cssref/css_selectors.asp"},
+          {name: "Flexbox", href: "https://css-tricks.com/snippets/css/a-guide-to-flexbox/"},
+          {name: "Colors", href: "https://en.wikipedia.org/wiki/Web_colors"},
+          {name: "ColorZilla", href: "https://chrome.google.com/webstore/detail/colorzilla/bhlhnicpbhignbdhedgjhgdocnmhomnp"}
         ]
       },
       {
-        title: 'Regex',
+        title: "Regex",
         links: [
-          {name: '101', href: 'https://regex101.com'},
-          {name: 'Tutorial', href: 'http://www.rexegg.com'},
-          {name: 'Cheat Sheet', href: 'https://regexcheatsheet.com'}
+          {name: "101", href: "https://regex101.com"},
+          {name: "Tutorial", href: "http://www.rexegg.com"},
+          {name: "Cheat Sheet", href: "https://regexcheatsheet.com"}
         ]
       }
     ]
   },
   doc: {
-    head: 'Documentation',
+    head: "Documentation",
     a: {
-      text: 'Platform',
-      link: 'https://community.commandersact.com/platform'
+      text: "Platform",
+      link: "https://community.commandersact.com/platform"
     },
     b: {
-      text: 'TAG',
-      link: 'https://community.commandersact.com/tagcommander'
+      text: "TAG",
+      link: "https://community.commandersact.com/tagcommander"
     },
     c: {
-      text: 'TRUST',
-      link: 'https://community.commandersact.com/trustcommander'
+      text: "TRUST",
+      link: "https://community.commandersact.com/trustcommander"
     },
     d: {
-      text: 'MIX',
-      link: 'https://community.commandersact.com/mixcommander'
+      text: "MIX",
+      link: "https://community.commandersact.com/mixcommander"
     },
     e: {
-      text: 'DATA',
-      link: 'https://community.commandersact.com/datacommander'
+      text: "DATA",
+      link: "https://community.commandersact.com/datacommander"
     },
     f: {
-      text: 'FUSE',
-      link: '#fuse'
+      text: "FUSE",
+      link: "#fuse"
     },
     cta: {
-      text: 'Interne',
-      href: 'https://tagcommander.atlassian.net/wiki/spaces/CAH/overview?mode=global'
+      text: "Interne",
+      href: "https://tagcommander.atlassian.net/wiki/spaces/CAH/overview?mode=global"
     }
   },
   about: {
     main: {
-      head: 'À propos de ce site',
-      body: 'Veuillez noter que ce site est destiné uniquement à pratiquer l\'implémentation des différents produits Commanders Act et n\'est en aucun cas une vitrine officielle. Le code de ce site n\'est pas non plus lié aux technologies Commanders Act mais à un projet de développement Web personnel.'
+      head: "À propos de ce site",
+      body: "Veuillez noter que ce site est destiné uniquement à pratiquer l'implémentation des différents produits Commanders Act et n'est en aucun cas une vitrine officielle. Le code de ce site n'est pas non plus lié aux technologies Commanders Act mais à un projet de développement Web personnel."
     },
     hosting: {
-      text: 'Hébergé sur GitHub Pages',
-      href: 'https://docs.github.com/en/free-pro-team@latest/github/working-with-github-pages/about-github-pages'
+      text: "Hébergé sur GitHub Pages",
+      href: "https://docs.github.com/en/free-pro-team@latest/github/working-with-github-pages/about-github-pages"
     },
     sub: {
-      head: 'Commanders Act',
-      video: 'https://www.youtube.com/embed/6S9Es0UhzQg',
+      head: "Commanders Act",
+      video: "https://www.youtube.com/embed/6S9Es0UhzQg",
       cta: {
-        text: 'Plus d\'information',
-        href: 'https://www.commandersact.com'
+        text: "Plus d'information",
+        href: "https://www.commandersact.com"
       }
     }
 
