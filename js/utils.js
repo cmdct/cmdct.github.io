67d3a85a7b23c10186e702ce5afc38e9e793ce1c
cmdct <sylvain.demaimay@commandersact.com>
2020-12-31 17:40:35 +0100

    Add utils

---
+++ js/utils.js
@@ -0,0 +1,16 @@
+const utils = {
+  waitThreeSecondsIf: (getExpected, callback) => {
+    let i = 0;
+    const max = 12
+        , milliseconds = 250
+        , check = setInterval(() => {
+            if (getExpected()) {
+              clearInterval(check);
+              callback()
+            }
+            else if (i >= max) clearInterval(check);
+            else i++
+          }, milliseconds)
+        ;
+  }
+};
