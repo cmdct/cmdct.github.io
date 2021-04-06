cba350259489dff2495a4f7b906b0aa0cdfa75da
cmdct <sylvain.demaimay@commandersact.com>
2021-04-06 21:24:50 +0200

    Update Content

---
+++ js/content-041.js
@@ -0,0 +1,402 @@
+const content = {
+  home: {
+    name: `Desk`,
+    cards: [
+      {
+        name: `IT watch`,
+        links: [
+          {
+            name: `BDM`,
+            href: `https://www.blogdumoderateur.com/`
+          },
+          {
+            name: `Silicon`,
+            href: `https://www.silicon.fr/`
+          },
+          {
+            name: `Next Inpact`,
+            href: `https://www.nextinpact.com/`
+          },
+          {
+            name: `L'Usine Digitale`,
+            href: `https://www.usine-digitale.fr/`
+          },
+          {
+            name: `GitHub Trending`,
+            href: `https://github.com/trending`
+          },
+          {
+            name: `Developpez`,
+            href: `https://www.developpez.com/`
+          },
+        ]
+      },
+      {
+        name: `Commanders Act`,
+        links: [
+          {
+            name: `Platform`,
+            href: `https://platform.commandersact.com`
+          },
+          {
+            name: `Extension`,
+            href: `https://chrome.google.com/webstore/detail/commanders-act-assistant/lfaifjhjdolnpnlgeohohaalbeidhlpj?hl=fr`
+          },
+          {
+            name: `Community`,
+            href: `https://community.commandersact.com/en`
+          },
+          {
+            name: `Corporate`,
+            href: `https://www.commandersact.com`
+          },
+          {
+            name: `GitHub`,
+            href: `https://github.com/TagCommander`
+          }
+        ]
+      },
+      {
+        name: `Internal`,
+        links: [
+          {
+            name: `Lucca`,
+            href: `https://fjordtechnologies.ilucca.net/home`
+          },
+          {
+            name: `Zoho`,
+            href: `https://projects.zoho.eu`
+          },
+          {
+            name: `Slack`,
+            href: `https://tagcoteam.slack.com`
+          },
+          {
+            name: `Drive`,
+            href: `https://drive.google.com/drive/my-drive`
+          },
+          {
+            name: `Office`,
+            href: `https://login.microsoftonline.com`
+          },
+          {
+            name: `Dropbox`,
+            href: `https://www.dropbox.com/home`
+          }
+        ]
+      },
+      {
+        name: `Language`,
+        links: [
+          {
+            name: `Tech`,
+            href: `https://techterms.com`
+          },
+          {
+            name: `Marketing`,
+            href: `https://www.definitions-marketing.com`
+          },
+          {
+            name: `Conjugueur`,
+            href: `https://www.gymglish.com/fr/conjugaison/vatefaireconjuguer`
+          },
+          {
+            name: `Wiktionary`,
+            href: `https://www.wiktionary.org`
+          },
+          {
+            name: `Wikipedia`,
+            href: `https://www.wikipedia.org`
+          },
+          {
+            name: `Translate`,
+            href: `https://translate.google.com/?ui=tob&sl=en&tl=fr&op=translate`
+          }
+        ]
+      },
+      {
+        name: `Code`,
+        links: [
+          {
+            name: `Beautifier`,
+            href: `https://beautifier.io`
+          },
+          {
+            name: `DiffNow`,
+            href: `https://www.diffnow.com/compare-clips`
+          },
+          {
+            name: `Codeshare`,
+            href: `https://codeshare.io`
+          },
+          {
+            name: `Byte Counter`,
+            href: `https://mothereff.in/byte-counter`
+          }
+        ]
+      },
+      {
+        name: `Regex`,
+        links: [
+          {
+            name: `101`,
+            href: `https://regex101.com`
+          },
+          {
+            name: `Tutorial`,
+            href: `http://www.rexegg.com`
+          },
+          {
+            name: `Cheat Sheet`,
+            href: `https://regexcheatsheet.com`
+          },
+          {
+            name: `RegExr`,
+            href: `https://regexr.com`
+          }
+        ]
+      },
+      {
+        name: `JavaScript`,
+        links: [
+          {
+            name: `MDN`,
+            href: `https://developer.mozilla.org/en-US/docs/Web/JavaScript`
+          },
+          {
+            name: `Eloquent`,
+            href: `https://eloquentjavascript.net`
+          },
+          {
+            name: `ECMAScript`,
+            href: `https://www.ecma-international.org/ecma-262/11.0/index.html#title`
+          }
+        ]
+      },
+      {
+        name: `CSS`,
+        links: [
+          {
+            name: `Selectors`,
+            href: `https://www.w3schools.com/cssref/css_selectors.asp`
+          },
+          {
+            name: `Flexbox`,
+            href: `https://css-tricks.com/snippets/css/a-guide-to-flexbox`
+          },
+          {
+            name: `Colors`,
+            href: `https://en.wikipedia.org/wiki/Web_colors`
+          },
+          {
+            name: `ColorZilla`,
+            href: `https://chrome.google.com/webstore/detail/colorzilla/bhlhnicpbhignbdhedgjhgdocnmhomnp`
+          },
+          {
+            name: `Hex to RGB`,
+            href: `https://www.rapidtables.com/convert/color/hex-to-rgb.html`
+          },
+          {
+            name: `Web Safe Fonts`,
+            href: `https://www.w3schools.com/cssref/css_websafe_fonts.asp`
+          }
+        ]
+      },
+      {
+        name: `Font`,
+        links: [
+          {
+            name: `Base64 Transfonter`,
+            href: `https://transfonter.org/`
+          },
+          {
+            name: `Web Safe Fonts`,
+            href: `https://www.w3schools.com/cssref/css_websafe_fonts.asp`
+          },
+          {
+            name: `Google Fonts`,
+            href: `https://fonts.google.com/`
+          }
+        ]
+      },
+      {
+        name: `Image`,
+        links: [
+          {
+            name: `Base64 Encoder`,
+            href: `https://www.base64-image.de`
+          },
+          {
+            name: `iconmonstr`,
+            href: `https://iconmonstr.com`
+          },
+          {
+            name: `Unsplash`,
+            href: `https://unsplash.com`
+          }
+        ]
+      },
+      {
+        name: `Network`,
+        links: [
+          {
+            name: `Status Codes`,
+            href: `https://developer.mozilla.org/en-US/docs/Web/HTTP/Status`
+          },
+          {
+            name: `Internet Speed`,
+            href: `https://fast.com`
+          },
+          {
+            name: `Check IP`,
+            href: `http://checkip.amazonaws.com`
+          },
+          {
+            name: `Masque`,
+            href: `https://cric.grenoble.cnrs.fr/Administrateurs/Outils/CalculMasque`
+          }
+        ]
+      },
+      {
+        name: `Google Chrome`,
+        links: [
+          {
+            name: `DevTools`,
+            href: `https://developers.google.com/web/tools/chrome-devtools`
+          },
+          {
+            name: `Debugging`,
+            href: `https://developers.google.com/web/tools/chrome-devtools/javascript/reference`
+          },
+          {
+            name: `Local Overrides`,
+            href: `https://developers.google.com/web/updates/2018/01/devtools#overrides`
+          },
+          {
+            name: `Web Fundamentals`,
+            href: `https://developers.google.com/web/fundamentals`
+          },
+          {
+            name: `Resource Override`,
+            href: `https://chrome.google.com/webstore/detail/resource-override/pkoacgokdfckfpndoffpifphamojphii`
+          },
+          {
+            name: `Wappalyzer`,
+            href: `https://chrome.google.com/webstore/detail/wappalyzer/gppongmhjkpfnbhagpmjfkannfbllamg/related`
+          }
+        ]
+      },
+      {
+        name: `Google Analytics`,
+        links: [
+          {
+            name: `Measurement Protocol`,
+            href: `https://developers.google.com/analytics/devguides/collection/protocol/v1/parameters`
+          },
+          {
+            name: `Analytics Academy`,
+            href: `https://analytics.google.com/analytics/academy/`
+          },
+          {
+            name: `GTM Help`,
+            href: `https://support.google.com/tagmanager/?hl=en#topic=3441530`
+          },
+          {
+            name: `GA Help`,
+            href: `https://support.google.com/analytics/?hl=en#topic=9143232`
+          },
+          {
+            name: `GTM`,
+            href: `https://tagmanager.google.com/#/home`
+          },
+          {
+            name: `GA`,
+            href: `https://analytics.google.com`
+          }
+        ]
+      }
+    ]
+  },
+  doc: {
+    name: `Documentation`,
+    a: {
+      name: `Platform`,
+      href: `https://community.commandersact.com/platform`
+    },
+    b: {
+      name: `TAG`,
+      href: `https://community.commandersact.com/tagcommander`
+    },
+    c: {
+      name: `TRUST`,
+      href: `https://community.commandersact.com/trustcommander`
+    },
+    d: {
+      name: `MIX`,
+      href: `https://community.commandersact.com/mixcommander`
+    },
+    e: {
+      name: `DATA`,
+      href: `https://community.commandersact.com/datacommander`
+    },
+    f: {
+      name: `FUSE`,
+      href: `#fuse`
+    },
+    cta: {
+      name: `Internal`,
+      href: `https://tagcommander.atlassian.net/wiki/spaces/CAH/overview?mode=global`
+    }
+  },
+  about: {
+    main: {
+      name: `About`,
+      body: `Please note that this site is intended only to practice the implementation of the various Commanders Act products and is by no means an official showcase. The code on this site is also not related to Commanders Act technologies but to a personal web development project. The data collected is only used to verify the configuration.`
+    },
+    hosting: {
+      name: `Hosted on GitHub Pages`,
+      href: `https://docs.github.com/en/free-pro-team@latest/github/working-with-github-pages/about-github-pages`
+    },
+    sub: {
+      name: `Commanders Act`,
+      video: `https://www.youtube.com/embed/6S9Es0UhzQg`,
+      cta: {
+        name: `More information`,
+        href: `https://www.commandersact.com`
+      }
+    }
+  },
+  default: `This page does not exist`,
+  site: {
+    name: `Cmd`,
+    footer: {
+      texts: [
+        {
+          name: `GitHub Pages`,
+          href: `https://pages.github.com`
+        }
+      ],
+      icons: [
+        {
+          name: `Github`,
+          href: `https://github.com/cmdct/cmdct.github.io`,
+          src: `images/iconmonstr-github.png`
+        }
+      ]
+    }
+  }
+};
+content.site.nav = [
+  {
+    name: content.home.name,
+    href: `#home`
+  },
+  {
+    name: content.doc.name,
+    href: `#documentation`
+  },
+  {
+    name: content.about.main.name,
+    href: `#about`
+  }
+];
